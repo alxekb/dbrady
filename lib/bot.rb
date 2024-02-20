b221c00e86ff5c28456fc0e27d0c0b220dad207d
Aleksei Ivanov <aleksei.ivanov@appsolute-mobility.com>
2024-02-20 16:45:27 +0100

    Fix requirement statements

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,9 +3,4 @@
 require 'date'
 require 'nokogiri'
 
-require 'bot/com_thetrainline/com_thetrainline'
-require 'bot/com_thetrainline/adapter'
-require 'bot/com_thetrainline/http_transport'
-require 'bot/com_thetrainline/fare'
-require 'bot/com_thetrainline/segment'
-require 'bot/com_thetrainline/builders/segment_builder'
+Dir.glob('./lib/**/*.rb').each { |file| require file }
